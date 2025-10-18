6463eacb30df5f24b5c9c63dfa1791f79badf9a7
Jora <91777313+Jora001@users.noreply.github.com>
2025-10-18 22:14:48 +0400

    Added section

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import dynamic from "next/dynamic";
 import Section2 from "./components/Section2";
 import Section3 from "./components/Section3";
 // import Section5 from "./components/Section5";
-// import Section4 from "./components/Section4";
+import Section4 from "./components/Section4";
 import Section5 from "./components/Section5";
 import Section6 from "./components/Section6";
 import Section7 from "./components/Section7";
@@ -17,7 +17,7 @@ export default function Page() {
       <HeroSection />
       <Section2 />
       <Section3 />
-      {/* <Section4 /> */}
+      <Section4 />
       <Section5/>
       <Section6/>
       <Section7/>    
